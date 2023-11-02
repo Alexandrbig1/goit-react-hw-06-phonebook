3f13965abb0399f6251b3e8a95cc8bfdb5fb3191
Alexandrbig1 <alexsmagin1@gmail.com>
2023-11-01 23:41:15 -0500

    updated

---
+++ src/redux/store.js
@@ -0,0 +1,22 @@
+// import { combineReducers, configureStore } from "@reduxjs/toolkit";
+// import { persistReducer } from "redux-persist";
+
+// const contactsReducer = (state = [], action) => {};
+// const filterReducer = (state = [], action) => {};
+
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   whiteList: ["contacts"],
+// };
+
+// const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+//   filter: filterReducer,
+// });
+
+// const persistedRootReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = configureStore({
+//   reducer: persistedRootReducer,
+// });
